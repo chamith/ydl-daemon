7feca404b4d5f137be041e9a4150bd5d588a87ba
Chamith Siriwardena <chamith@gmail.com>
2020-05-18 21:31:28 +0530

    copying the web-ui files

--- update.sh
+++ update.sh
@@ -3,9 +3,9 @@
 SVC_NAME=youtube-dl-daemon
 SRC_NAME=app.py
 SCRIPT_DIR=$(dirname "$0")
-BIN_DIR=/usr/local/bin
+LIB_DIR=/var/lib/$SVC_NAME
 SVC_DIR=/etc/systemd/system
-BIN_FILE_TARGET=$BIN_DIR/$SVC_NAME
+BIN_FILE_TARGET=$LIB_DIR/$SVC_NAME
 
 
 if [ -f "$SVC_DIR/$SVC_NAME.service" ]; then
@@ -14,6 +14,9 @@ if [ -f "$SVC_DIR/$SVC_NAME.service" ]; then
 
     echo "Updating the binaries"
     cp $SCRIPT_DIR/$SRC_NAME $BIN_FILE_TARGET
+
+    echo "Updating the web ui files"
+    cp $SCRIPT_DIR/web-ui $LIB_DIR/ -r
 
     echo "Starting the service $SVC_NAME"
     systemctl start $SVC_NAME
