3d5124345dbf2964437b7793128bcbc45b059fb9
Chamith Siriwardena <chamith@gmail.com>
2020-05-19 09:03:19 +0530

    adding ui files back

---
+++ web-ui/precache-manifest.b76acd7dc93935134cb7785440b4b37b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "310f2a1331ce41987e17765b6dd687fc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4c1c49cab4a99825f34f",
+    "url": "/static/css/main.d8a0fef6.chunk.css"
+  },
+  {
+    "revision": "db98d811100c8e22a09a",
+    "url": "/static/js/2.2400da49.chunk.js"
+  },
+  {
+    "revision": "531d7b2065717e21d8dbdd1394bf24a4",
+    "url": "/static/js/2.2400da49.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c1c49cab4a99825f34f",
+    "url": "/static/js/main.72bf54c9.chunk.js"
+  },
+  {
+    "revision": "d87a8fbfa2ae2979d906",
+    "url": "/static/js/runtime-main.5e4a42a2.js"
+  }
+]);
