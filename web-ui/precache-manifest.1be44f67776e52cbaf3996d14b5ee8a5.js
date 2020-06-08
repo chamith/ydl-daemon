e41038b53da8b12a21e46c2f6360f7fc2d6186b2
Chamith Siriwardena <chamith@gmail.com>
2020-06-08 07:09:44 +0530

    ui changes to conditionally render based on the request type

--- web-ui/precache-manifest.1be44f67776e52cbaf3996d14b5ee8a5.js
+++ web-ui/precache-manifest.1be44f67776e52cbaf3996d14b5ee8a5.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "85b95d92951c535586bbe3b3d957329a",
+    "revision": "047ed68b433ee1b88c23e4e818390404",
     "url": "/index.html"
   },
   {
-    "revision": "75789816de6c5298f0e4",
+    "revision": "7ed13d76c08ececc80c6",
     "url": "/static/css/main.04c7de8e.chunk.css"
   },
   {
-    "revision": "17a182ad4c774e15cb63",
-    "url": "/static/js/2.ec21d9dd.chunk.js"
+    "revision": "dbd3efe207bde15d728a",
+    "url": "/static/js/2.f5d49da1.chunk.js"
   },
   {
     "revision": "0749163b59fbee32225059cb60c18af6",
-    "url": "/static/js/2.ec21d9dd.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.f5d49da1.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "75789816de6c5298f0e4",
-    "url": "/static/js/main.aac28bc5.chunk.js"
+    "revision": "7ed13d76c08ececc80c6",
+    "url": "/static/js/main.ca338dc2.chunk.js"
   },
   {
     "revision": "d87a8fbfa2ae2979d906",
--- web-ui/service-worker.js
+++ web-ui/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3393f1875ccc68ef8a71128123aaf326.js"
+  "/precache-manifest.1be44f67776e52cbaf3996d14b5ee8a5.js"
 );
 
 self.addEventListener('message', (event) => {
