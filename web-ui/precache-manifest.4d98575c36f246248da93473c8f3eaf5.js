94b6c454506b070acc5d0781ada2e4575fca985b
Chamith Siriwardena <chamith@gmail.com>
2020-06-09 21:15:05 +0530

    ui changes with useEffect

--- web-ui/precache-manifest.4d98575c36f246248da93473c8f3eaf5.js
+++ web-ui/precache-manifest.4d98575c36f246248da93473c8f3eaf5.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "a77e2a1d2fd338e1f1cbe964ba975ade",
+    "revision": "adac8c14290223751e4817a22c818d28",
     "url": "/index.html"
   },
   {
-    "revision": "1dbccb32dd12908e20ba",
-    "url": "/static/js/2.847883be.chunk.js"
+    "revision": "b013eddd5d004e27284b",
+    "url": "/static/js/2.d24a684a.chunk.js"
   },
   {
     "revision": "0749163b59fbee32225059cb60c18af6",
-    "url": "/static/js/2.847883be.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.d24a684a.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "c5ff29181a19f2b62ed6",
-    "url": "/static/js/main.aad7d53f.chunk.js"
+    "revision": "d0ebe60914baf9184f8b",
+    "url": "/static/js/main.42d69967.chunk.js"
   },
   {
     "revision": "d87a8fbfa2ae2979d906",
--- web-ui/service-worker.js
+++ web-ui/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1c4e859a036a6cd8c416df6d8e4ad90d.js"
+  "/precache-manifest.4d98575c36f246248da93473c8f3eaf5.js"
 );
 
 self.addEventListener('message', (event) => {
